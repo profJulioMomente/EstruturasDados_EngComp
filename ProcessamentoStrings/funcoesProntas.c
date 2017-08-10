de062c04a04de5e828172847d5e637aa5911cdbb
Prof. Julio Momente <prof.juliomomente@gmail.com>
2017-08-10 19:37:46 -0300

    Inclusão de funções
    Inclusão das funções da biblioteca string.h

--- ProcessamentoStrings/funcoesProntas.c
+++ ProcessamentoStrings/funcoesProntas.c
@@ -9,15 +9,21 @@ void converteMinusculo(char *S, int tam);
 void converteInteiro(char *S);
 void converteReal(char *S);
 void converteString(char *S);
+void copia(char *Destino, char *Origem);
+void comparacao(char *Str1, char *Str2);
+void juntaStrings(char *Primeira, char *Segunda);
 
 main () {
-	char Texto[200];
+	char Texto[200], Txt2[200];
 	char Inteiro[10], Real[10], Res[10];
 	
 	setlocale(LC_ALL, "portuguese");
 	
-	//puts("Informe um texto.");
-	//gets(Texto);
+	puts("Informe um texto.");
+	gets(Texto);
+	
+	puts("Informe outro texto.");
+	gets(Txt2);
 	
 	//converteMaiusculo(Texto);
 	
@@ -27,8 +33,16 @@ main () {
 	
 	//converteInteiro(Inteiro);
 	//converteReal(Real);
-	converteString(Res);
+	//converteString(Res);
 	
+	//copia(Txt2, Texto);
+	
+	//comparacao(Texto, Txt2);
+	
+	juntaStrings(Texto, Txt2);
+	
+	//puts(Txt2);
+	puts("\n\n");
 	system("pause");
 	
 }
@@ -86,4 +100,38 @@ void converteString(char *S){
 	for (i = 0; i<strlen(S); i++){
 		printf("S[%d] = %c\n", i, S[i]);
 	}
+}
+
+void copia(char *Destino, char *Origem) {
+	
+	strcpy(Destino, Origem);
+}
+
+void comparacao(char *Str1, char *Str2){
+	int resultado;
+	
+	resultado = strcmp(Str1, Str2);
+	
+	if( resultado < 0 ) {
+		puts("Str1 vem antes de Str2");
+		puts(Str1);
+		puts(Str2);
+	} else if( resultado > 0) {
+		puts("Str2 vem antes de Str1");
+		puts(Str2);
+		puts(Str1);
+	} else { //resultado == 0
+		puts("Str1 e identica a Str2");
+		puts(Str1);
+		puts(Str2);
+	}
+	
+}
+
+void juntaStrings(char *Primeira, char *Segunda){
+	//Junta a segunda no final da primeira
+	
+	strcat(Primeira, Segunda);
+	
+	puts(Primeira);
 }
